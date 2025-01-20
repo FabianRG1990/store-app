33eadf2d59160ec7ef76186df0f268fb1dc6e065
FabianRG1990 <103287298+FabianRG1990@users.noreply.github.com>
2025-01-19 23:32:31 -0600

    feat: simplificacion del selector

--- store/storeLib/src/lib/globalStore/global-state.selectors.ts
+++ store/storeLib/src/lib/globalStore/global-state.selectors.ts
@@ -1,8 +1,4 @@
 import { createSelector } from '@ngrx/store';
 import { AppState } from './app.state';
 
-// Selector para obtener "globalState" del estado global
-export const selectGlobalState = createSelector(
-  (state: AppState) => state.globalState,
-  (globalState) => globalState
-);
+export const selectGlobalState = createSelector((state: AppState) => state.globalState, (s) => s);
