c5f40be49df8e01ec726a50c1a6eabfe288b1ebc
FabianRG1990 <103287298+FabianRG1990@users.noreply.github.com>
2025-01-29 11:35:21 -0600

    feat: crear el reducer

--- store/storeLib/src/lib/globalStore/store/global-state.reducer.ts
+++ store/storeLib/src/lib/globalStore/store/global-state.reducer.ts
@@ -1,9 +1,51 @@
 import { createReducer, on } from '@ngrx/store';
 import { changeState } from './global-state.actions';
+import { AppState } from './app.state';
 
 export const initialGlobalState = 0;
+
+export const initialState: AppState = {
+  key1: 'Initial Key 0',
+  key2: 0,
+  key3: false,
+};
+
+
 
 export const globalStateReducer = createReducer(
   initialGlobalState,
   on(changeState, (state, { newState }) => newState) // Actualizamos el estado con el valor proporcionado
+);
+
+
+
+
+export const appReducer = createReducer(
+  initialState,
+
+  // Actualizar key1
+  on(updateKey1, (state, { key1 }) => ({
+    ...state,
+    key1,
+  })),
+
+  // Actualizar key2
+  on(updateKey2, (state, { key2 }) => ({
+    ...state,
+    key2,
+  })),
+
+  // Actualizar key3
+  on(updateKey3, (state, { key3 }) => ({
+    ...state,
+    key3,
+  })),
+
+  // Actualizar todos los keys
+  on(updateAllKeys, (state, { key1, key2, key3 }) => ({
+    ...state,
+    key1,
+    key2,
+    key3,
+  }))
 );
