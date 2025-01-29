ca0a8141cbd43709695d0b102ca441b4dc504018
FabianRG1990 <103287298+FabianRG1990@users.noreply.github.com>
2025-01-29 11:19:20 -0600

    feat: state

--- store/storeLib/src/lib/globalStore/store/app.state.ts
+++ store/storeLib/src/lib/globalStore/store/app.state.ts
@@ -2,7 +2,9 @@ import { globalStateReducer } from "./global-state.reducer";
 
 export interface AppState {
   globalState: number;
-
+  key1: string;
+  key2: number;
+  key3: boolean;
 }
 
 export const appReducers = {
