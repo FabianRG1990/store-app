688abd8429d8c30ec7130ca944974efd4c2a92e4
FabianRG1990 <103287298+FabianRG1990@users.noreply.github.com>
2025-01-29 11:58:13 -0600

    feat: logica de selectors

--- store/storeLib/src/lib/globalStore/store/global-state.selectors.ts
+++ store/storeLib/src/lib/globalStore/store/global-state.selectors.ts
@@ -2,3 +2,7 @@ import { createSelector } from '@ngrx/store';
 import { AppState } from './app.state';
 
 export const selectGlobalState = createSelector((state: AppState) => state.globalState, (s) => s);
+
+
+
+export const selecKey1 = createSelector((state: AppState) => state.key1, (s) => s);
